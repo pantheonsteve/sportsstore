7dd55412805888b54c65ef2b9987076a252facd5
stevebresnick <stevebresnick@gmail.com>
2019-09-11 17:40:34 -0400

    Files through Page 108

---
+++ src/data/placeholderData.js
@@ -0,0 +1,10 @@
+export const data = {
+  categories: ["Watersports", "Soccer", "Chess", "Running"],
+  products: [
+    {id: 1, name: "P1", category: "Watersports", description: "P1 (Watersports)", Price: 3},
+    {id: 2, name: "P2", category: "Watersports", description: "P2 (Watersports)", Price: 4},
+    {id: 3, name: "P3", category: "Running", description: "P3 (Running)", Price: 5},
+    {id: 4, name: "P4", category: "Chess", description: "P4 (Chess)", Price: 6},
+    {id: 5, name: "P5", category: "Chess", description: "P5 (Chess)", Price: 7},
+  ]
+}
