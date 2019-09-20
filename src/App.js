f9e69b97091c733e1769dfa1709218da8f961b3e
stevebresnick <stevebresnick@gmail.com>
2019-09-19 21:39:25 -0400

    up to page 111 top

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Right Now!
         </a>
       </header>
     </div>
